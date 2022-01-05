311e3c1cdc702563f844a513270bd81011ed2534
Brenda Pereira <brendacamara@outlook.com>
2022-01-05 08:30:35 -0300

    chore: create migrations

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello wrld")
+import { MikroORM } from "@mikro-orm/core"
+import { __prod__ } from "./constants"
+import { Post } from "./entitites/Post"
+import microConfig from "./mikro-orm.config"
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig)
+
+  const post = orm.em.create(Post, { title: "My first post" })
+  await orm.em.persistAndFlush(post)
+}
+
+main()
